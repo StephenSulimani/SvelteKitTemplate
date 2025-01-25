f1070ea3efbfa0e54a993f2248e127689d24b565
StephenSulimani <stephensulimani@gmail.com>
2025-01-24 23:28:36 -0500

    :hammer: rearranged order of commands in start script

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cd /usr/src/app/build && bunx prisma generate || exit 1
 cd /usr/src/app/build && bun i -D prisma
+cd /usr/src/app/build && bunx prisma generate || exit 1
 bun /usr/src/app/build/index.js
